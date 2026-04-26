fb0c371efa8bda2ca4bd883e0447f104388dbedc
sameerkhann73 <khansameer786004@gmail.com>
2026-04-26 23:12:00 +0530

    Hypertuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,9 +42,32 @@ class ModelTrainer:
                 # "XGBRegressor":XGBRegressor(),
                 "CatBoosting Regressor":CatBoostRegressor(verbose=False)
             }
+            param={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbours Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance']
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                }
+            }
             model_report:dict=evaluate_models(X_train=X_train,
                                                y_train=y_train,X_test=X_test,
-                                               y_test=y_test,models=models)
+                                               y_test=y_test,models=models,param=param)
             
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
