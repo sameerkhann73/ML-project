3be1413ec44f39982f7cc2a43be65b6d5265b91c
sameerkhann73 <khansameer786004@gmail.com>
2026-04-26 17:01:24 +0530

    Model training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,9 @@ from dataclasses import dataclass
 from src.components.data_trans import DataTransformation
 from src.components.data_trans import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 
 
 
@@ -56,4 +59,6 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
